c4f0d3c5e34ea6f0e34fdaa090f353e7aa447541
nnature <nnature@163.com>
2015-10-29 22:03:57 +0800

    创建新的一行2

--- src/com/atguigu/git/Client.java
+++ src/com/atguigu/git/Client.java
@@ -5,6 +5,7 @@ public class Client {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new Client");
+		System.out.println("new line 2");
 	}
 
 }
